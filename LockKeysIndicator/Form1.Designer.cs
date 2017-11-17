32d3d6681b9507e0d58648344a0d38c23127d621
Janith Ganewatta <janithsg@gmail.com>
2017-11-17 11:05:03 +0530

    Add project files.

---
+++ LockKeysIndicator/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace LockKeysIndicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.rClickMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rClickMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.rClickMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            // 
+            // rClickMenu
+            // 
+            this.rClickMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitItem});
+            this.rClickMenu.Name = "rClickMenu";
+            this.rClickMenu.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitItem
+            // 
+            this.exitItem.Name = "exitItem";
+            this.exitItem.Size = new System.Drawing.Size(92, 22);
+            this.exitItem.Text = "Exit";
+            this.exitItem.Click += new System.EventHandler(this.exitItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 45);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "KeyLockStatus";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.rClickMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip rClickMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitItem;
+    }
+}
+
---
+++ LockKeysIndicator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LockKeysIndicator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                notifyIcon.Icon = Properties.Resources.a;
+                notifyIcon.Text = "CapsLock is ON";
+            }
+            if (!Control.IsKeyLocked(Keys.CapsLock))
+            {
+                notifyIcon.Icon = Properties.Resources.b;
+                notifyIcon.Text = "CapsLock is OFF";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            notifyIcon.Visible = true;
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
